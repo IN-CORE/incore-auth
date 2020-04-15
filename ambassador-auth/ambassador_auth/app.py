1f44f84d78f532b736775b04ca7086d09cc99a43
Diego Calderon <diegoac2@illinois.edu>
2020-04-15 17:24:22 -0500

    updated pep8 styling, limiting lines to 79 characters and docstrings to 72 characters

--- ambassador-auth/ambassador_auth/app.py
+++ ambassador-auth/ambassador_auth/app.py
@@ -6,7 +6,6 @@ from jose import jwt
 from jose.exceptions import JWTError, ExpiredSignatureError, JWTClaimsError
 from urllib.parse import unquote_plus
 
-
 config = {
     "PROTECTED_RESOURCES": ["dfr3", "data", "hazard", "space"],
     "GROUPS": {"incore_user": ["dfr3", "data", "hazard", "space"]}
@@ -20,18 +19,22 @@ app.config.from_mapping(config)
 @app.before_request
 def verify_token():
     """
-    This function distinguishes between requests that need authorization and verifies if those who need to be
-    authorized contain the access token in its headers or cookies. If the token verification and user
-    authorization was successful, it updates the headers, adding a user-info string.
-    :return: HTTP response.
-    200 if path is not protected.
-    200 if path is protected and meets the following criteria: 1) request contains an Authorization header or cookie
-    with bearer token. 2) The access token has a valid signature (not expired or invalid). 3) The user belongs to the
-    appropriate group required to access the protected path.
-    401 if token is invalid or not present.
-    403 if token is present and valid but the user does not belong to the appropriate groups for the protected path.
+    This function distinguishes between requests that need authorization
+    and verifies if those who need to be authorized contain the access
+    token in its headers or cookies. If the token verification and user
+    authorization was successful, it updates the headers, adding a
+    user-info string.
+    :return: HTTP response. 200 if path is not protected. 200 if path is
+    protected and meets the following criteria: 1) request contains an
+    Authorization header or cookie with bearer token. 2) The access
+    token has a valid signature (not expired or invalid). 3) The user
+    belongs to the appropriate group required to access the protected
+    path. 401 if token is invalid or not present. 403 if token is
+    present and valid but the user does not belong to the appropriate
+    groups for the protected path.
     """
-    # check if the url is for the /healthz route, in the future we might need to check what is the actual rule
+    # check if the url is for the /healthz route, in the future we might
+    # need to check what is the actual rule
     if request.url_rule is not None:
         return healthz()
 
@@ -48,9 +51,11 @@ def verify_token():
     # retrieve access token from header or cookies
     headers = {}
     if request.headers.get('Authorization') is not None:
-        headers['Authorization'] = unquote_plus(request.headers['Authorization'])
+        headers['Authorization'] = unquote_plus(
+            request.headers['Authorization'])
     elif request.cookies.get('Authorization') is not None:
-        headers['Authorization'] = unquote_plus(request.cookies['Authorization'])
+        headers['Authorization'] = unquote_plus(
+            request.cookies['Authorization'])
     else:
         response = make_response('Unauthorized', 401)
         return response
@@ -59,16 +64,18 @@ def verify_token():
     except IndexError:
         return make_response('Invalid token', 401)
     public_key = f"-----BEGIN PUBLIC KEY-----\n" \
-                 f"{str(os.environ.get('KEYCLOAK_PUBLIC_KEY'))}" \
-                 f"\n-----END PUBLIC KEY-----"
+        f"{str(os.environ.get('KEYCLOAK_PUBLIC_KEY'))}" \
+        f"\n-----END PUBLIC KEY-----"
 
     # decode token for validating its signature
     try:
         access_token = jwt.decode(access_token, public_key)
     except ExpiredSignatureError:
-        return make_response('JWT Expired Signature Error: token signature has expired', 401)
+        return make_response(
+            'JWT Expired Signature Error: token signature has expired', 401)
     except JWTClaimsError:
-        return make_response('JWT Claims Error: token signature is invalid', 401)
+        return make_response('JWT Claims Error: token signature is invalid',
+                             401)
     except JWTError:
         return make_response('JWT Error: token signature is invalid', 401)
 
@@ -78,7 +85,8 @@ def verify_token():
     except KeyError:
         return make_response('Invalid token', 401)
 
-    # get all resources the user can access to, based on the groups the user belongs to
+    # get all resources the user can access to, based on the groups
+    # the user belongs to
     user_accessible_resources = []
     for group in user_groups:
         user_accessible_resources.extend(app.config["GROUPS"][group])
